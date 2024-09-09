eb742ea820bdab57ad3d34e509ccd369987356c6
Seth Welch <sethwelch85@gmail.com>
2024-09-09 00:15:33 -0700

    Improving top bar

--- src/DrawerAppBar.tsx
+++ src/DrawerAppBar.tsx
@@ -10,19 +10,23 @@ export default function DrawerAppBar() {
       <AppBar component="nav">
         <Toolbar
           sx={{
-            minHeight: "80px !important",
-            height: "80px",
+            minHeight: "60px !important",
+            height: "60px",
             background: "linear-gradient(45deg, black, transparent)",
             display: "flex",
             justifyContent: "space-between",
-            px: { xs: 1, sm: 4 },
+            pl: { xs: 0, sm: 1 },
+            pr: { xs: 1, sm: 4 },
           }}
         >
-          <Link to="/" style={{ textDecoration: "none" }}>
+          <Link
+            to="/"
+            style={{ textDecoration: "none", display: "flex", marginRight: 4 }}
+          >
             <img
               style={{
                 width: "100%",
-                maxWidth: 400,
+                maxWidth: 350,
               }}
               src={`${logo}`}
               alt="logo"
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,15 +13,16 @@ function Button({ onClick, sx, children }: ButtonProps) {
         backgroundColor: "navy",
         border: "2px solid white",
         borderRadius: "4px",
-        padding: "8px 16px",
+        padding: "6px 12px",
         color: "white",
-        fontWeight: { xs: 500, sm: "bold" },
+        fontWeight: { xs: 500, sm: 600 },
         fontSize: { xs: ".75rem", sm: "1rem" },
         height: "fit-content",
         "&:hover": {
           backgroundColor: "rgba(0, 0, 128, 0.8)",
           color: "white",
         },
+        whiteSpace: "nowrap",
         ...sx,
       }}
       onClick={onClick}
