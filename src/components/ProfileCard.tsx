7aa152764a55092b511778007d7fa24bc73f47af
Seth Welch <sethwelch85@gmail.com>
2024-09-30 10:53:14 -0700

    Adding no wrap to name and title

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,44 +1,44 @@
-import { Box } from '@mui/material'
-import PersonIcon from '@mui/icons-material/Person'
+import { Box } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
 
 type ProfileCardProps = {
-  name: string
-  title: string
-  image?: string
-}
+  name: string;
+  title: string;
+  image?: string;
+};
 
 function ProfileCard({ name, title, image }: ProfileCardProps) {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: '#07355b',
-        padding: '16px 24px',
-        boxShadow: '5px 5px 5px grey',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#07355b",
+        padding: "16px 24px",
+        boxShadow: "5px 5px 5px grey",
       }}
     >
       <Box
         id="image-container"
         sx={{
-          background: 'lightBlue',
+          background: "lightBlue",
           borderRadius: 40,
-          display: 'flex',
-          width: 'fit-content',
+          display: "flex",
+          width: "fit-content",
           mb: 2,
         }}
       >
         {image ? (
           <img
             style={{
-              width: '100%',
+              width: "100%",
               maxWidth: 200,
-              borderTopLeftRadius: '50% 50%',
-              borderTopRightRadius: '50% 50%',
-              borderBottomRightRadius: '50% 50%',
-              borderBottomLeftRadius: '50% 50%',
+              borderTopLeftRadius: "50% 50%",
+              borderTopRightRadius: "50% 50%",
+              borderBottomRightRadius: "50% 50%",
+              borderBottomLeftRadius: "50% 50%",
             }}
             src={`${image}`}
             alt="placeholder"
@@ -49,7 +49,7 @@ function ProfileCard({ name, title, image }: ProfileCardProps) {
               height: 200,
               width: 200,
               fontSize: 120,
-              color: 'midnightblue',
+              color: "midnightblue",
             }}
           >
             <PersonIcon sx={{ fontSize: 200 }} />
@@ -59,21 +59,29 @@ function ProfileCard({ name, title, image }: ProfileCardProps) {
       <Box>
         <label
           style={{
-            width: '100%',
-            display: 'block',
+            width: "100%",
+            display: "block",
             fontSize: 20,
             fontWeight: 600,
-            color: 'white',
+            color: "white",
+            textWrap: "nowrap",
           }}
         >
           {name}
         </label>
-        <label style={{ width: '100%', display: 'block', color: 'white' }}>
+        <label
+          style={{
+            width: "100%",
+            display: "block",
+            color: "white",
+            textWrap: "nowrap",
+          }}
+        >
           {title}
         </label>
       </Box>
     </Box>
-  )
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
